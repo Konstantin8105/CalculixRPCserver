9118cc600c74564c352241b08a9480cf6b31d669
konstantin8105 <konstantin8105@gmail.com>
2017-04-13 00:13:27 +0300

    server reply to struct

--- serverCalculix/server.go
+++ serverCalculix/server.go
@@ -35,10 +35,22 @@ func NewCalculix() *Calculix {
 	return calculix
 }
 
-// AmountTasks - amount allowable tasks to sending for calculation
-func (c *Calculix) AmountTasks(empty string, amountFreeTasks *int) error {
+// Amount - amount
+type Amount struct {
+	A int
+}
+
+// MaxAllowableTasks - amount allowable tasks to sending for calculation
+func (c *Calculix) MaxAllowableTasks(empty string, amountFreeTasks *Amount) error {
 	_ = empty
-	amountFreeTasks = &c.amountTasks
+	amountFreeTasks.A = runtime.GOMAXPROCS(runtime.NumCPU())
+	return nil
+}
+
+// AmountTasks - amount allowable tasks to sending for calculation
+func (c *Calculix) AmountTasks(empty string, amountFreeTasks *Amount) error {
+	_ = empty
+	amountFreeTasks.A = c.amountTasks
 	return nil
 }
 
