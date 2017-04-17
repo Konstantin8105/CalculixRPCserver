83154ef62d4235e3f645c6f903f737e60c0a71c0
konstantin8105 <konstantin8105@gmail.com>
2017-04-17 22:10:15 +0300

    divide by 2  amount of proccesor for avoid extra heavy job

--- serverCalculix/task.go
+++ serverCalculix/task.go
@@ -10,7 +10,7 @@ type Amount struct {
 // MaxAllowableTasks - amount allowable tasks to sending for calculation
 func (c *Calculix) MaxAllowableTasks(empty string, amountFreeTasks *Amount) error {
 	_ = empty
-	amountFreeTasks.A = runtime.GOMAXPROCS(runtime.NumCPU())
+	amountFreeTasks.A = runtime.GOMAXPROCS(runtime.NumCPU()) / 2
 	return nil
 }
 
