6f1b94045e0f72a0558998052e41d3eb0186fa99
Konstantin8105 <pollygon2011>
2017-05-11 14:14:27 +0300

    fix: generation name

--- serverCalculix/serverName.go
+++ serverCalculix/serverName.go
@@ -1,8 +1,8 @@
 package serverCalculix
 
 import (
-	"fmt"
 	"math/rand"
+	"time"
 )
 
 // ServerName - name of server
@@ -15,7 +15,15 @@ func (c *Calculix) GetServerName(empty string, name *ServerName) error {
 	_ = empty
 	if len(c.serverName) == 0 {
 		c.mutex.Lock()
-		c.serverName = fmt.Sprintf("%v", rand.Int())
+		const charset = "abcdefghijklmnopqrstuvwxyz" +
+			"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+		var seededRand *rand.Rand = rand.New(
+			rand.NewSource(time.Now().UnixNano()))
+		b := make([]byte, 20)
+		for i := range b {
+			b[i] = charset[seededRand.Intn(len(charset))]
+		}
+		c.serverName = string(b)
 		c.mutex.Unlock()
 	}
 	name.A = c.serverName
